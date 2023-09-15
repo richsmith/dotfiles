0c38bb8398bac196f16eb4d4b2bbdde776236071
Rich Smith <rls@hwyl.org>
2023-09-15 11:31:44 +0100

    Enable Tree-Sitter

--- emacs/init.el
+++ emacs/init.el
@@ -326,6 +326,17 @@
   (interactive)
   (copilot-clear-overlay))
 
+;;
+;; **** Languages ****
+;;
+(use-package tree-sitter-langs
+  :ensure t)
+(use-package tree-sitter
+  :ensure t
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
 
 ;; **** Python ****
 ;;
