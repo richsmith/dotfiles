f08c557753d4e387aaba17e551460780058bcba7
Rich Smith <rls@hwyl.org>
2024-04-01 09:53:04 +0100

    Init file locating function update

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -9,7 +9,7 @@
 
 ;; Define initialisation files
 (setq init-dir (file-name-concat user-emacs-directory "init"))
-(defvar init-file-names
+(defvar init-org-file-names
   '("core.org"
     "display.org"
     "navigation.org"
@@ -20,26 +20,28 @@
   "List of org initialisation file names to load.")
 (when (eq system-type 'darwin)
   (add-to-list 'init-file-names "mac.org"))
-(defvar init-files
+(defvar init-org-files
   (mapcar (lambda (file-name) (expand-file-name file-name init-dir))
           init-file-names)
   "List of org files to load.")
 
-;; Provision for opening initialisation files
-(defun find-init ()
-  "Open an init file."
-  (interactive)
-  (let ((file (completing-read "Select init file: " init-files)))
-    (when file
-      (find-file file))))
-
 ;; Load the initialisation files
-(defun load-init-file (file-name)
+(defun load-init-org-file (file-name)
   (org-babel-load-file (expand-file-name file-name init-dir)))
-(mapc 'load-init-file init-files)
+(mapc 'load-init-org-file init-org-files)
 
 ;; Load machine-localised settings if present
 ;; (Do this after everything else so that it can override settings)
 (let ((local-file (expand-file-name "local.el" user-emacs-directory)))
   (when (file-exists-p local-file)
     (load local-file)))
+
+;; Provision for opening initialisation files
+(defvar init-files
+  (cons user-init-file init-org-files))
+(defun find-init ()
+  "Open an init file."
+  (interactive)
+  (let ((file (completing-read "Select init file: " init-files)))
+    (when file
+      (find-file file))))
